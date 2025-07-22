b4a32092ace9056ae55197962d58f8b0f11a3878
Calum Price <calumaprice@gmail.com>
2025-07-22 19:37:54 +0100

    modified kitty and nvim colorschemes to kanagawa

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,7 +1,25 @@
+--return {
+--  "pineapplegiant/spaceduck",
+--  branch = "main",
+--  opts = function()
+--    vim.cmd("colorscheme spaceduck")
+--  end
+--}
+
+--return {
+--	"rose-pine/neovim",
+--	name = "rose-pine",
+--	config = function()
+--		vim.cmd("colorscheme rose-pine")
+--	end
+--}
+
 return {
-  "pineapplegiant/spaceduck",
-  branch = "main",
-  opts = function()
-    vim.cmd("colorscheme spaceduck")
-  end
+  "thesimonho/kanagawa-paper.nvim",
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme("kanagawa-paper-ink")
+  end,
+  opts = {...},
 }
